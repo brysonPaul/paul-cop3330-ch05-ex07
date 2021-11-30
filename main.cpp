2676d8cde41464aadacb12611a854c3a56177d58
Bryson Paul <89651994+brysonPaul@users.noreply.github.com>
2021-11-30 03:46:03 +0000

    fixed problem

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ void quadratic(int a,int b, int c){
 	}
 	else if (discriminant == 0)
 	{
-		result1 = (-1*b) / (2*a);
+		double bot = 2*a ; 
+		double top = -1*b ;
+		result1 = top/bot;
 		cout << "one real root, which is " << result1;	
 	}
 	else if(discriminant > 0){
